e9f24672191281ed67188ac0f697a1d9d2dffe41
Rami Alkawadri <ramialkawadri@hotmail.com>
2022-09-09 18:01:04 +0200

    Adding spinner

--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,20 @@ import { rootReducer } from './root-reducer';
 import persistReducer from 'redux-persist/es/persistReducer';
 import persistStore from 'redux-persist/es/persistStore';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleware = [process.env.NODE_ENV === 'development' && logger].filter(
-    Boolean
-);
+const middleware = [
+    process.env.NODE_ENV === 'development' && logger,
+    thunk,
+].filter(Boolean);
 
 // Root reducer
 export const store = configureStore({
